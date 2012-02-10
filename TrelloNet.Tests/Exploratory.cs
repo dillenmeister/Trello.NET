757535b3ba747edc16bd5bad0d482976d2dbbc67
Oskar Dillen <dillenmeister@gmail.com>
2012-02-10 08:55:42 +0100

    Emptied exploratory tests

--- TrelloNet.Tests/Exploratory.cs
+++ TrelloNet.Tests/Exploratory.cs
@@ -4,23 +4,11 @@ namespace TrelloNet.Tests
 {
 	[TestFixture]
 	public class Exploratory
-	{
-		private ITrelloToBe _trello;
-
+	{		
 		[Test, Explicit]
 		public void Demonstrate_Functionality()
 		{
 			
 		}
-	}
-
-	public interface ITrelloToBe
-	{
-		IMembers Members { get; }
-		IBoards Boards { get; }
-		ILists Lists { get; }
-		ICards Cards { get; }
-		IChecklists Checklists { get; }
-		IOrganizations Organizations { get;}
-	}
+	}	
 }
