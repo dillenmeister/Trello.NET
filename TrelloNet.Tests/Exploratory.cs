4dc9a1b6e22b43bee421ea7052301eaf423a551c
Oskar Dillen <dillenmeister@gmail.com>
2012-02-10 08:51:35 +0100

    Removed some unnecessary using statements

--- TrelloNet.Tests/Exploratory.cs
+++ TrelloNet.Tests/Exploratory.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using TrelloNet.Internal;
 
 namespace TrelloNet.Tests
 {
--- TrelloNet/UnauthorizedException.cs
+++ TrelloNet/UnauthorizedException.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace TrelloNet
 {
 	public class UnauthorizedException : TrelloException
