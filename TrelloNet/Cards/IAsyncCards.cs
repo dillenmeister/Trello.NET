51f60c1f4deaafcbed0a0703dd617265513ee584
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:38:03 +0100

    Documentation for Cards

--- TrelloNet/Cards/IAsyncCards.cs
+++ TrelloNet/Cards/IAsyncCards.cs
@@ -6,28 +6,119 @@ namespace TrelloNet
 {
 	public interface IAsyncCards
 	{
+		/// <summary>
+		/// GET /cards/[card_id]
+		/// </summary>
 		Task<Card> WithId(string cardId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards/[idCard]
+		/// </summary>
 		Task<Card> WithShortId(int shortId, IBoardId boardId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards
+		/// </summary>
 		Task<IEnumerable<Card>> ForBoard(IBoardId board, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /lists/[list_id]/cards
+		/// </summary>
 		Task<IEnumerable<Card>> ForList(IListId list, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/cards
+		/// </summary>
 		Task<IEnumerable<Card>> ForMember(IMemberId member, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /members/me/cards
+		/// </summary>
 		Task<IEnumerable<Card>> ForMe(CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /checklists/[checklist_id]/cards
+		/// </summary>
 		Task<IEnumerable<Card>> ForChecklist(IChecklistId checklist, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// POST /cards
+		/// </summary>
 		Task<Card> Add(NewCard card);
+
+		/// <summary>
+		/// POST /cards
+		/// </summary>
 		Task<Card> Add(string name, IListId list);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]
+		/// </summary>
 		Task Delete(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/closed
+		/// </summary>
 		Task Archive(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/closed
+		/// </summary>
 		Task SendToBoard(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/desc
+		/// </summary>
 		Task ChangeDescription(ICardId card, string description);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/name
+		/// </summary>
 		Task ChangeName(ICardId card, string name);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/due
+		/// </summary>
 		Task ChangeDueDate(ICardId card, DateTime? due);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/idList
+		/// </summary>
 		Task Move(ICardId card, IListId list);
+
+		/// <summary>
+		/// POST /cards/[card_id]/labels
+		/// </summary>
 		Task AddLabel(ICardId card, Color color);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/labels/[color]
+		/// </summary>
 		Task RemoveLabel(ICardId card, Color color);
+
+		/// <summary>
+		/// POST /cards/[card_id]/members
+		/// </summary>
 		Task AddMember(ICardId card, IMemberId member);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/members/[idMember]
+		/// </summary>
 		Task RemoveMember(ICardId card, IMemberId member);
+
+		/// <summary>
+		/// POST /cards/[card_id]/actions/comment
+		/// </summary>
 		Task AddComment(ICardId card, string comment);
+
+		/// <summary>
+		/// POST /cards/[card_id]/checklists
+		/// </summary>
 		Task AddChecklist(ICardId card, IChecklistId checklist);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/checklists/[idChecklist]
+		/// </summary>
 		Task RemoveChecklist(ICardId card, IChecklistId checklist);
 	}
 }
--- TrelloNet/Cards/ICards.cs
+++ TrelloNet/Cards/ICards.cs
@@ -5,28 +5,119 @@ namespace TrelloNet
 {
 	public interface ICards
 	{
+		/// <summary>
+		/// GET /cards/[card_id]
+		/// </summary>
 		Card WithId(string cardId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards/[idCard]
+		/// </summary>
 		Card WithShortId(int shortId, IBoardId boardId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards
+		/// </summary>
 		IEnumerable<Card> ForBoard(IBoardId board, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /lists/[list_id]/cards
+		/// </summary>
 		IEnumerable<Card> ForList(IListId list, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/cards
+		/// </summary>
 		IEnumerable<Card> ForMember(IMemberId member, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /members/me/cards
+		/// </summary>
 		IEnumerable<Card> ForMe(CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// GET /checklists/[checklist_id]/cards
+		/// </summary>
 		IEnumerable<Card> ForChecklist(IChecklistId checklist, CardFilter filter = CardFilter.Default);
+
+		/// <summary>
+		/// POST /cards
+		/// </summary>
 		Card Add(NewCard card);
+
+		/// <summary>
+		/// POST /cards
+		/// </summary>
 		Card Add(string name, IListId list);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]
+		/// </summary>
 		void Delete(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/closed
+		/// </summary>
 		void Archive(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/closed
+		/// </summary>
 		void SendToBoard(ICardId card);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/desc
+		/// </summary>
 		void ChangeDescription(ICardId card, string description);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/name
+		/// </summary>
 		void ChangeName(ICardId card, string name);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/due
+		/// </summary>
 		void ChangeDueDate(ICardId card, DateTime? due);
+
+		/// <summary>
+		/// PUT /cards/[card_id]/idList
+		/// </summary>
 		void Move(ICardId card, IListId list);
+
+		/// <summary>
+		/// POST /cards/[card_id]/labels
+		/// </summary>
 		void AddLabel(ICardId card, Color color);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/labels/[color]
+		/// </summary>
 		void RemoveLabel(ICardId card, Color color);
+
+		/// <summary>
+		/// POST /cards/[card_id]/members
+		/// </summary>
 		void AddMember(ICardId card, IMemberId member);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/members/[idMember]
+		/// </summary>
 		void RemoveMember(ICardId card, IMemberId member);
+
+		/// <summary>
+		/// POST /cards/[card_id]/actions/comment
+		/// </summary>
 		void AddComment(ICardId card, string comment);
+
+		/// <summary>
+		/// POST /cards/[card_id]/checklists
+		/// </summary>
 		void AddChecklist(ICardId card, IChecklistId checklist);
+
+		/// <summary>
+		/// DELETE /cards/[card_id]/checklists/[idChecklist]
+		/// </summary>
 		void RemoveChecklist(ICardId card, IChecklistId checklist);
 	}
 }
