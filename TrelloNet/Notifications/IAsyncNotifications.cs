b5c5b994110258e60440c5d5d14f6d88f38bd2f1
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:50:39 +0100

    Documentation for Notifications

--- TrelloNet/Notifications/IAsyncNotifications.cs
+++ TrelloNet/Notifications/IAsyncNotifications.cs
@@ -6,7 +6,14 @@ namespace TrelloNet
 {
 	public interface IAsyncNotifications
 	{
+		/// <summary>
+		/// GET /notifications/[notification_id]
+		/// </summary>
 		Task<Notification> WithId(string notificationId);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/notifications
+		/// </summary>
 		Task<IEnumerable<Notification>> ForMe(IEnumerable<NotificationType> types = null, ReadFilter readFilter = ReadFilter.Default, Paging paging = null);
 	}
 }
--- TrelloNet/Notifications/INotifications.cs
+++ TrelloNet/Notifications/INotifications.cs
@@ -5,7 +5,14 @@ namespace TrelloNet
 {
 	public interface INotifications
 	{
+		/// <summary>
+		/// GET /notifications/[notification_id]
+		/// </summary>
 		Notification WithId(string notificationId);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/notifications
+		/// </summary>
 		IEnumerable<Notification> ForMe(IEnumerable<NotificationType> types = null, ReadFilter readFilter = ReadFilter.Default, Paging paging = null);
 	}
 }
