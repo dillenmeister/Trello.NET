2c8f3c144cbbf8366219e25c6f6bbd111b3dc913
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:26:44 +0100

    Documentation for Boards

--- TrelloNet/Boards/IAsyncBoards.cs
+++ TrelloNet/Boards/IAsyncBoards.cs
@@ -5,18 +5,69 @@ namespace TrelloNet
 {
 	public interface IAsyncBoards
 	{
+		/// <summary>
+		/// GET /boards/[board_id]
+		/// </summary>	
 		Task<Board> WithId(string boardId);
+
+		/// <summary>
+		/// GET /cards/[card_id]/board
+		/// </summary>
 		Task<Board> ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /checklists/[checklist_id]/board
+		/// </summary>
 		Task<Board> ForChecklist(IChecklistId checklist);
+
+		/// <summary>
+		/// GET /lists/[list_id]/board
+		/// </summary>
 		Task<Board> ForList(IListId list);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/boards
+		/// </summary>
 		Task<IEnumerable<Board>> ForMember(IMemberId member, BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// GET /members/me/boards
+		/// </summary>
 		Task<IEnumerable<Board>> ForMe(BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// GET /organizations/[org_id or name]/boards
+		/// </summary>
 		Task<IEnumerable<Board>> ForOrganization(IOrganizationId organization, BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// POST /boards
+		/// </summary>
 		Task<Board> Add(NewBoard board);
+
+		/// <summary>
+		/// POST /boards
+		/// </summary>
 		Task<Board> Add(string name);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/closed
+		/// </summary>
 		Task Close(IBoardId board);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/closed
+		/// </summary>
 		Task ReOpen(IBoardId board);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/name
+		/// </summary>
 		Task ChangeName(IBoardId board, string name);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/desc
+		/// </summary>
 		Task ChangeDescription(IBoardId board, string description);
 	}
 }
--- TrelloNet/Boards/IBoards.cs
+++ TrelloNet/Boards/IBoards.cs
@@ -4,18 +4,69 @@ namespace TrelloNet
 {
 	public interface IBoards
 	{
+		/// <summary>
+		/// GET /boards/[board_id]
+		/// </summary>	
 		Board WithId(string boardId);
+
+		/// <summary>
+		/// GET /cards/[card_id]/board
+		/// </summary>
 		Board ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /checklists/[checklist_id]/board
+		/// </summary>
 		Board ForChecklist(IChecklistId checklist);
+
+		/// <summary>
+		/// GET /lists/[list_id]/board
+		/// </summary>
 		Board ForList(IListId list);
+
+		/// <summary>
+		/// GET /members/[member_id or username]/boards
+		/// </summary>
 		IEnumerable<Board> ForMember(IMemberId member, BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// GET /members/me/boards
+		/// </summary>
 		IEnumerable<Board> ForMe(BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// GET /organizations/[org_id or name]/boards
+		/// </summary>
 		IEnumerable<Board> ForOrganization(IOrganizationId organization, BoardFilter filter = BoardFilter.Default);
+
+		/// <summary>
+		/// POST /boards
+		/// </summary>
 		Board Add(NewBoard board);
+
+		/// <summary>
+		/// POST /boards
+		/// </summary>
 		Board Add(string name);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/closed
+		/// </summary>
 		void Close(IBoardId board);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/closed
+		/// </summary>
 		void ReOpen(IBoardId board);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/name
+		/// </summary>
 		void ChangeName(IBoardId board, string name);
+
+		/// <summary>
+		/// PUT /boards/[board_id]/desc
+		/// </summary>
 		void ChangeDescription(IBoardId board, string description);
 	}
 }
