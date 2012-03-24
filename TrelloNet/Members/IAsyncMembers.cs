7d8ec294f61bc0037de03bec616920423356ac4c
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:48:52 +0100

    Documentation for Members

--- TrelloNet/Members/IAsyncMembers.cs
+++ TrelloNet/Members/IAsyncMembers.cs
@@ -5,11 +5,34 @@ namespace TrelloNet
 {
 	public interface IAsyncMembers
 	{
+		/// <summary>
+		/// GET /members/[member_id or username]
+		/// </summary>
 		Task<Member> WithId(string memberIdOrUsername);
+
+		/// <summary>
+		/// GET /members/me
+		/// </summary>
 		Task<Member> Me();
+
+		/// <summary>
+		/// GET /boards/[board_id]/members
+		/// </summary>
 		Task<IEnumerable<Member>> ForBoard(IBoardId board, MemberFilter filter = MemberFilter.Default);
+
+		/// <summary>
+		/// GET /cards/[card_id]/members
+		/// </summary>
 		Task<IEnumerable<Member>> ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /organizations/[org_id or name]/members
+		/// </summary>
 		Task<IEnumerable<Member>> ForOrganization(IOrganizationId organization, MemberFilter filter = MemberFilter.Default);
+
+		/// <summary>
+		/// /tokens/[token]/member
+		/// </summary>
 		Task<Member> ForToken(string token);
 	}
 }
--- TrelloNet/Members/IMembers.cs
+++ TrelloNet/Members/IMembers.cs
@@ -4,11 +4,34 @@ namespace TrelloNet
 {
 	public interface IMembers
 	{
+		/// <summary>
+		/// GET /members/[member_id or username]
+		/// </summary>
 		Member WithId(string memberIdOrUsername);
+
+		/// <summary>
+		/// GET /members/me
+		/// </summary>
 		Member Me();
+
+		/// <summary>
+		/// GET /boards/[board_id]/members
+		/// </summary>
 		IEnumerable<Member> ForBoard(IBoardId board, MemberFilter filter = MemberFilter.Default);
+
+		/// <summary>
+		/// GET /cards/[card_id]/members
+		/// </summary>
 		IEnumerable<Member> ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /organizations/[org_id or name]/members
+		/// </summary>
 		IEnumerable<Member> ForOrganization(IOrganizationId organization, MemberFilter filter = MemberFilter.Default);
+
+		/// <summary>
+		/// /tokens/[token]/member
+		/// </summary>
 		Member ForToken(string token);
 	}
 }
