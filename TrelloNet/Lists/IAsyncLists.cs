b308dbb1b3b0078c6ac5f8b10106fa2a422e4ee7
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:45:51 +0100

    Documentation for Lists

--- TrelloNet/Lists/IAsyncLists.cs
+++ TrelloNet/Lists/IAsyncLists.cs
@@ -5,13 +5,44 @@ namespace TrelloNet
 {
 	public interface IAsyncLists
 	{
+		/// <summary>
+		/// GET /lists/[list_id]
+		/// </summary>
 		Task<List> WithId(string listId);
+
+		/// <summary>
+		/// GET /cards/[card_id]/list
+		/// </summary>
 		Task<List> ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards
+		/// </summary>
 		Task<IEnumerable<List>> ForBoard(IBoardId board, ListFilter filter = ListFilter.Default);
+
+		/// <summary>
+		/// POST /lists
+		/// </summary>
 		Task<List> Add(NewList list);
+
+		/// <summary>
+		/// POST /lists
+		/// </summary>
 		Task<List> Add(string name, IBoardId board);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/closed
+		/// </summary>
 		Task Archive(IListId list);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/closed
+		/// </summary>
 		Task SendToBoard(IListId list);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/name
+		/// </summary>
 		Task ChangeName(IListId list, string name);
 	}
 }
--- TrelloNet/Lists/ILists.cs
+++ TrelloNet/Lists/ILists.cs
@@ -4,13 +4,44 @@ namespace TrelloNet
 {
 	public interface ILists
 	{
+		/// <summary>
+		/// GET /lists/[list_id]
+		/// </summary>
 		List WithId(string listId);
+
+		/// <summary>
+		/// GET /cards/[card_id]/list
+		/// </summary>
 		List ForCard(ICardId card);
+
+		/// <summary>
+		/// GET /boards/[board_id]/cards
+		/// </summary>
 		IEnumerable<List> ForBoard(IBoardId board, ListFilter filter = ListFilter.Default);
+
+		/// <summary>
+		/// POST /lists
+		/// </summary>
 		List Add(NewList list);
+
+		/// <summary>
+		/// POST /lists
+		/// </summary>
 		List Add(string name, IBoardId board);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/closed
+		/// </summary>
 		void Archive(IListId list);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/closed
+		/// </summary>
 		void SendToBoard(IListId list);
+
+		/// <summary>
+		/// PUT /lists/[list_id]/name
+		/// </summary>
 		void ChangeName(IListId list, string name);
 	}
 }
