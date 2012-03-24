42f3889d0091fc1de5f57aa1e5c3b24309be847d
Oskar Dillen <dillenmeister@gmail.com>
2012-03-24 17:42:03 +0100

    Documentation for Checklists

--- TrelloNet/Checklists/IAsyncChecklists.cs
+++ TrelloNet/Checklists/IAsyncChecklists.cs
@@ -5,12 +5,39 @@ namespace TrelloNet
 {
 	public interface IAsyncChecklists
 	{
+		/// <summary>
+		/// GET /checklists/[checklist_id]
+		/// </summary>
 		Task<Checklist> WithId(string checklistId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/checklists
+		/// </summary>
 		Task<IEnumerable<Checklist>> ForBoard(IBoardId board);
+
+		/// <summary>
+		/// GET /cards/[card_id]/checklists
+		/// </summary>
 		Task<IEnumerable<Checklist>> ForCard(ICardId card);
+
+		/// <summary>
+		/// POST /checklists
+		/// </summary>
 		Task<Checklist> Add(string name, IBoardId board);
+
+		/// <summary>
+		/// PUT /checklists/[checklist_id]/name
+		/// </summary>
 		Task ChangeName(IChecklistId checklist, string name);
+
+		/// <summary>
+		/// POST /checklists/[checklist_id]/checkitems
+		/// </summary>
 		Task AddCheckItem(IChecklistId checklist, string name);
+
+		/// <summary>
+		/// DELETE /checklists/[checklist_id]/checkitems/[idCheckItem]
+		/// </summary>
 		Task RemoveCheckItem(IChecklistId checklist, string checkItemId);
 	}
 }
--- TrelloNet/Checklists/IChecklists.cs
+++ TrelloNet/Checklists/IChecklists.cs
@@ -4,12 +4,39 @@ namespace TrelloNet
 {
 	public interface IChecklists
 	{
+		/// <summary>
+		/// GET /checklists/[checklist_id]
+		/// </summary>
 		Checklist WithId(string checklistId);
+
+		/// <summary>
+		/// GET /boards/[board_id]/checklists
+		/// </summary>
 		IEnumerable<Checklist> ForBoard(IBoardId board);
+
+		/// <summary>
+		/// GET /cards/[card_id]/checklists
+		/// </summary>
 		IEnumerable<Checklist> ForCard(ICardId card);
+
+		/// <summary>
+		/// POST /checklists
+		/// </summary>
 		Checklist Add(string name, IBoardId board);
+
+		/// <summary>
+		/// PUT /checklists/[checklist_id]/name
+		/// </summary>
 		void ChangeName(IChecklistId checklist, string name);
+
+		/// <summary>
+		/// POST /checklists/[checklist_id]/checkitems
+		/// </summary>
 		void AddCheckItem(IChecklistId checklist, string name);
+
+		/// <summary>
+		/// DELETE /checklists/[checklist_id]/checkitems/[idCheckItem]
+		/// </summary>
 		void RemoveCheckItem(IChecklistId checklist, string checkItemId);
 	}
 }
