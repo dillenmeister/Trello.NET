fa58f70b44ac158fad799daa47839c7cf07b692a
Oskar Dillen <dillenmeister@gmail.com>
2012-03-27 15:14:01 +0200

    Some docs for organizations

--- TrelloNet/Organizations/IOrganizations.cs
+++ TrelloNet/Organizations/IOrganizations.cs
@@ -6,21 +6,29 @@ namespace TrelloNet
 	{
 		/// <summary>
 		/// GET /organizations/[org_id or name]
+		/// <br/>
+		/// Required permissions: read
 		/// </summary>
 		Organization WithId(string orgIdOrName);
 
 		/// <summary>
 		/// GET /boards/[board_id]/organization
+		/// <br/>
+		/// Required permissions: read
 		/// </summary>
 		Organization ForBoard(IBoardId board);
 
 		/// <summary>
 		/// GET /members/[member_id or username]/organizations
+		/// <br/>
+		/// Required permissions: read
 		/// </summary>
 		IEnumerable<Organization> ForMember(IMemberId member, OrganizationFilter filter = OrganizationFilter.Default);
 
 		/// <summary>
 		/// GET /members/me/organizations
+		/// <br/>
+		/// Required permissions: read
 		/// </summary>
 		IEnumerable<Organization> ForMe(OrganizationFilter filter = OrganizationFilter.Default);
 	}
