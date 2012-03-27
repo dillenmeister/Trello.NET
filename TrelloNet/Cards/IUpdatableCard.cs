6905b93ab97508808dfce5a36e5e5d6aefeca6ae
Oskar Dillen <dillenmeister@gmail.com>
2012-03-27 18:08:04 +0200

    No need for IUpdatableCard to have setters

--- TrelloNet/Cards/IUpdatableCard.cs
+++ TrelloNet/Cards/IUpdatableCard.cs
@@ -4,12 +4,11 @@ namespace TrelloNet
 {
 	public interface IUpdatableCard
 	{
-		string Id { get; set; }
-		string Name { get; set; }
-		string Desc { get; set; }
-		bool Closed { get; set; }
-		string IdList { get; set; }
-		DateTime? Due { get; set; }
-
+		string Id { get; }
+		string Name { get; }
+		string Desc { get; }
+		bool Closed { get; }
+		string IdList { get; }
+		DateTime? Due { get; }
 	}
 }
