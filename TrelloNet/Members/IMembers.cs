5704a71c3aa63b78f24ebe36a81b401f5ef00854
Oskar Dillen <dillenmeister@gmail.com>
2012-03-27 15:09:23 +0200

    Some more docs for Member

--- TrelloNet/Members/IMembers.cs
+++ TrelloNet/Members/IMembers.cs
@@ -6,31 +6,44 @@ namespace TrelloNet
 	{
 		/// <summary>
 		/// GET /members/[member_id or username]
+		/// <br/>
+		/// Required permissions: read
 		/// </summary>
 		Member WithId(string memberIdOrUsername);
 
 		/// <summary>
 		/// GET /members/me
+		/// <br/>
+		/// This call will respond as if you had supplied the username associated with the supplied token.
+		/// Required permissions: read.
 		/// </summary>
 		Member Me();
 
 		/// <summary>
 		/// GET /boards/[board_id]/members
+		/// <br/>
+		/// Required permissions: read.
 		/// </summary>
 		IEnumerable<Member> ForBoard(IBoardId board, MemberFilter filter = MemberFilter.Default);
 
 		/// <summary>
 		/// GET /cards/[card_id]/members
+		/// <br/>
+		/// Required permissions: read.
 		/// </summary>
 		IEnumerable<Member> ForCard(ICardId card);
 
 		/// <summary>
 		/// GET /organizations/[org_id or name]/members
+		/// <br/>
+		/// Required permissions: read.
 		/// </summary>
 		IEnumerable<Member> ForOrganization(IOrganizationId organization, MemberFilter filter = MemberFilter.Default);
 
 		/// <summary>
-		/// /tokens/[token]/member
+		/// GET /tokens/[token]/member
+		/// <br/>
+		/// Required permissions: read.
 		/// </summary>
 		Member ForToken(string token);
 	}
