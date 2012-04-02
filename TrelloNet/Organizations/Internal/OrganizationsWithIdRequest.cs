76cc9e7c657817000e3bb50b82c157d451f3be94
Oskar Dillen <dillenmeister@gmail.com>
2012-04-02 13:19:07 +0200

    Reverted a previous fix. Still doesn't work, but this is how it should be.

--- TrelloNet/Organizations/Internal/OrganizationsWithIdRequest.cs
+++ TrelloNet/Organizations/Internal/OrganizationsWithIdRequest.cs
@@ -3,9 +3,7 @@ namespace TrelloNet.Internal
 	internal class OrganizationsWithIdRequest : OrganizationsRequest
 	{
 		public OrganizationsWithIdRequest(string orgIdOrName) : base(orgIdOrName)
-		{
-			// Bug in Trello API: We have to ask for both desc and description.
-			AddParameter("fields", "name,displayName,url,desc,description");
+		{			
 		}
 	}
 }
