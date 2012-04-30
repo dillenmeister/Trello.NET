d04dc1bc7a653af3db9e4c7b3ecc9b0030dfa8c4
Oskar Dillen <dillenmeister@gmail.com>
2012-04-30 07:44:46 +0200

    The async bug is resolved in RestSharp 103.1.0-beta

---
+++ TrelloNet.Tests/AsyncTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TrelloNet.Tests
+{
+	[TestFixture]
+	public class AsyncTests : TrelloTestBase
+	{
+		[Test, Explicit]
+		public void ExecuteAsync_Bug_In_RestSharp()
+		{
+			var task = _trelloReadOnly.Async.Members.Me();
+			var tookLessThan5Seconds = task.Wait(5000);
+
+			Assert.That(tookLessThan5Seconds);
+		}
+	}
+}
